f82771c6b21071f288f0d8d266ca9298869044ff
Pyrite32 <patrick@method93.com>
2023-02-04 22:56:44 -0500

    ALMOST prevented the pot from clipping into oblivion

--- Scripts/Sprout.gd
+++ Scripts/Sprout.gd
@@ -85,10 +85,10 @@ func _physics_process(delta):
 	acceleration *= Acceleration
 	acceleration += Vector2.DOWN * GRAVITY
 	velocity += acceleration
-	var opposingForce = Vector2(-velocity.x * potSlowdown, 0.0)
+	var opposingForce = Vector2(-velocity.x * potSlowdown, -velocity.y * potSlowdown)
 	if potRef != null and potSlowdown > 0.0:
-		var pot_to_player : Vector2 = (Vector2(global_position.x, 0) - Vector2(potRef.global_position.x, 0)).normalized()
-		var opposingForceStrength = max(0.0, pot_to_player.dot(Vector2.RIGHT * -sign(velocity.x)) )
+		var pot_to_player : Vector2 = (Vector2(global_position.x, global_position.y) - Vector2(potRef.global_position.x, potRef.global_position.y)).normalized()
+		var opposingForceStrength = max(0.0, pot_to_player.dot(Vector2(-sign(velocity.x), -sign(velocity.y))) )
 		print("VELOCITY BEFORE: ", velocity)
 		velocity += opposingForce * opposingForceStrength
 		print("VELOCITY AFTER: ", velocity)
